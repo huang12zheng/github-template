5584e322470819c7a763a4e34f08d04d6feccf05
huang12zheng <805104533@qq.com>
2023-06-11 12:18:56 +0800

    1. include readme.md error 2. no use homepage 3. use once_cell::sync::Lazy;

--- {{project-name}}/src/prelude.rs
+++ {{project-name}}/src/prelude.rs
@@ -4,6 +4,6 @@ pub use envy::from_env;
 pub use eyre::{bail, eyre, Result};
 pub use itertools::Itertools;
 // pub use monostate::MustBe;
-pub use once_cell::Lazy;
+pub use once_cell::sync::Lazy;
 pub use serde::{Deserialize, Serialize};
 // pub use lazy_static::lazy_static;
